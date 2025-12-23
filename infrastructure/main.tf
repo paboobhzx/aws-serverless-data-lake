41802d2bd7ad29cfce83ef9d8fb1793c11364e54
Pablo <pablobhz@gmail.com>
2025-12-23 16:55:55 -0300

    Updating Terraform File

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -86,3 +86,50 @@ terraform {
 #Outputs
 output "ecr_repo_url" { value = aws_ecr_repository.etl_repo.repository_url }
 output "raw_bucket_name" { value = aws_s3_bucket.raw_bucket.id}
+#Data Transformation Steps and resources
+#AWS Glue Data Catalog
+resource "aws_glue_catalog_database" "datalake_db" {
+    name = "pablo_datalake_db"
+}
+#AWS Glue Crawler (discover the schema)
+resource "aws_glue_crawler" "datalake_crawler" {
+    name = "pablo_sales_crawler"
+    database_name = aws_glue_catalog_database.datalake_db.name 
+    role = aws_iam_role.glue_role.arn 
+
+    s3_target {
+        path = "s3://${aws_s3_bucket.clean_bucket.id}/"
+    }
+}
+#IAM Role for Glue
+resource "aws_iam_role" "glue_role" {
+    name = "serverless_glue_role"
+    assume_role_policy = jsonencode({
+        Version = "2012-10-17",
+        Statement = [{ 
+            Action = "sts:AssumeRole",
+            Effect = "Allow",
+            Principal = { Service = "glue.amazonaws.com"}
+        }]
+    })
+}
+
+resource "aws_iam_role_policy" "glue_policy" {
+    name = "serverless_glue_policy"
+    role = aws_iam_role.glue_role.id 
+    policy = jsonencode({ 
+        Version = "2012-10-17"
+        Statement = [{ 
+            Effect = "Allow",
+            Actions = ["s3:GetObject", "s3:ListBucket"],
+            Resource = [ 
+                aws_s3_bucket.clean_bucket.arn ,
+                "${aws_s3_bucket.clean_bucket.arn}/*"
+            ]
+        }, {
+            Effect = "Allow",
+            Action = ["glue:*", "logs:*"],
+            Resource = "*"
+        }]
+    })
+}
